3053b1cacd9bb4bf371d0924e09db25eb3c52951
Patrick Hettich <patrick_hettich@hotmail.com>
2022-06-26 09:12:35 +0200

    updated react index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import App from './App';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
-
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
